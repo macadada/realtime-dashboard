d4f59e271c92834c29adf5739662d1e3f4d252ad
Cameron King <cyking1233@gmail.com>
2024-12-20 18:17:55 -0500

    Deploy to Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/header";
 import { cn } from "@/lib/utils";
@@ -12,10 +12,6 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "OpenAI Realtime WebRTC",
--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -1,38 +1,33 @@
 "use client";
 
 import {
-  Sheet,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { MenuIcon } from "lucide-react";
 import Link from "next/link";
 import { siteConfig } from "@/config/site";
-import { usePathname } from "next/navigation";
 import { Badge } from "./ui/badge";
 
 export function MobileNav() {
-  const pathname = usePathname();
   const [open, setOpen] = useState(false);
 
   return (
     <div className="md:hidden flex gap-2 w-full items-center overflow-auto">
-      <Sheet open={open} onOpenChange={setOpen}>
-        <SheetTrigger asChild>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
           <Button size="icon" variant="outline" aria-label="Open navigation">
             <MenuIcon />
           </Button>
-        </SheetTrigger>
-        <SheetContent
-          side="left"
-          className="h-screen w-full rounded-none flex flex-col flex-1 justify-start items-start border-none overflow-y-auto p-6 pb-48"
-        >
-          <SheetHeader className="w-full">
-            <SheetTitle className="w-full text-left text-2xl font-bold">
+        </DialogTrigger>
+        <DialogContent className="w-[95vw] rounded-lg flex flex-col flex-1 justify-start items-start overflow-y-auto p-6">
+          <DialogHeader className="w-full">
+            <DialogTitle className="w-full text-left text-2xl font-bold">
               <Link
                 href="/"
                 onClick={() => setOpen(false)}
@@ -43,10 +38,14 @@ export function MobileNav() {
                   Beta
                 </Badge>
               </Link>
-            </SheetTitle>
-          </SheetHeader>
-        </SheetContent>
-      </Sheet>
+            </DialogTitle>
+          </DialogHeader>
+          <h1 className="mt-6 text-xl font-bold">About</h1>
+          <p className="mt-2 text-muted-foreground text-start text-lg">
+               This is a project that aims to demonstrate how to use OpenAI Realtime API with WebRTC in a modern Next 15 project. It has shadcn/ui components already installed and the WebRTC audio session hook already implemented. Clone the project and define your own tools.
+          </p>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
